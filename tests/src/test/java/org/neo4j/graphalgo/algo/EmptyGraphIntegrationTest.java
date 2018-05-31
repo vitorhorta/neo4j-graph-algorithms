aa1dee2705c0058d8b29b74311ad4345341b7a9e
Mark Needham <mark.needham@neotechnology.com>
2018-05-31 14:48:16 +0100

    more wip

--- tests/src/test/java/org/neo4j/graphalgo/algo/EmptyGraphIntegrationTest.java
+++ tests/src/test/java/org/neo4j/graphalgo/algo/EmptyGraphIntegrationTest.java
@@ -25,7 +25,7 @@ import org.neo4j.graphalgo.*;
 import org.neo4j.graphalgo.impl.ForwardBackwardScc;
 import org.neo4j.graphalgo.impl.betweenness.BetweennessCentrality;
 import org.neo4j.graphdb.Result;
-import org.neo4j.kernel.api.exceptions.KernelException;
+import org.neo4j.internal.kernel.api.exceptions.KernelException;
 import org.neo4j.kernel.impl.proc.Procedures;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 
