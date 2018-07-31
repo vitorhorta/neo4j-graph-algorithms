e9011dd68aabb5a9c00613c0b6aad10c8cb6565f
Mark Needham <m.h.needham@gmail.com>
2018-07-31 12:40:35 +0100

    add docs and lookup by nodeIds

--- algo/src/main/java/org/neo4j/graphalgo/GetNodeFunc.java
+++ algo/src/main/java/org/neo4j/graphalgo/GetNodeFunc.java
@@ -26,9 +26,11 @@ import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class GetNodeFunc {
-
-
     @Context
     public GraphDatabaseAPI api;
 
@@ -40,6 +42,12 @@ public class GetNodeFunc {
         } catch (NotFoundException e) {
             return null;
         }
+    }
+
+    @UserFunction("algo.getNodesById")
+    @Description("CALL algo.getNodesById(values) - return node for nodeIds. empty if none exists")
+    public List<Node> getNodesById(@Name(value = "nodeIds") List<Number> nodeIds) {
+        return nodeIds.stream().map(this::getNodeById).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
 }
