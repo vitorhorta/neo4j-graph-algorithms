e441c1099795c3f3e163e2c392995e40b7ebf85e
Mark Needham <mark.needham@neotechnology.com>
2018-09-24 10:49:22 +0100

    write version

--- algo/src/main/java/org/neo4j/graphalgo/similarity/BucketProc.java
+++ algo/src/main/java/org/neo4j/graphalgo/similarity/BucketProc.java
@@ -19,17 +19,26 @@
 package org.neo4j.graphalgo.similarity;
 
 import info.debatty.java.lsh.LSHSuperBit;
+import org.neo4j.graphalgo.api.IdMapping;
+import org.neo4j.graphalgo.core.IdMap;
 import org.neo4j.graphalgo.core.ProcedureConfiguration;
-import org.neo4j.procedure.Description;
-import org.neo4j.procedure.Mode;
-import org.neo4j.procedure.Name;
-import org.neo4j.procedure.Procedure;
+import org.neo4j.graphalgo.core.neo4jview.DirectIdMapping;
+import org.neo4j.graphalgo.core.utils.Pools;
+import org.neo4j.graphalgo.core.utils.ProgressTimer;
+import org.neo4j.graphalgo.core.utils.TerminationFlag;
+import org.neo4j.graphalgo.core.write.Exporter;
+import org.neo4j.graphalgo.core.write.Translators;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.procedure.*;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 public class BucketProc extends SimilarityProc {
+
+    @Context
+    public GraphDatabaseAPI dbAPI;
 
     @Procedure(name = "algo.similarity.bucket.stream", mode = Mode.READ)
     @Description("CALL algo.similarity.bucket.stream([{source:id, weights:[weights]}], {similarityCutoff:-1,degreeCutoff:0}) " +
@@ -57,29 +66,47 @@ public class BucketProc extends SimilarityProc {
     @Procedure(name = "algo.similarity.bucket", mode = Mode.WRITE)
     @Description("CALL algo.similarity.cosine([{item:id, weights:[weights]}], {similarityCutoff:-1,degreeCutoff:0}) " +
             "YIELD p50, p75, p90, p99, p999, p100 - computes cosine similarities")
-    public Stream<SimilaritySummaryResult> bucket(
+    public Stream<BucketSummaryResult> bucket(
             @Name(value = "data", defaultValue = "null") List<Map<String, Object>> data,
             @Name(value = "config", defaultValue = "{}") Map<String, Object> config) {
-
-        SimilarityComputer<WeightedInput> computer = (s,t,cutoff) -> s.cosineSquares(cutoff, t);
 
         ProcedureConfiguration configuration = ProcedureConfiguration.create(config);
 
         WeightedInput[] inputs = prepareWeights(data, getDegreeCutoff(configuration));
 
-        double similarityCutoff = getSimilarityCutoff(configuration);
-        // as we don't compute the sqrt until the end
-        if (similarityCutoff > 0d) similarityCutoff *= similarityCutoff;
+        int n = inputs[0].weights.length;
 
-        int topN = getTopN(configuration);
-        int topK = getTopK(configuration);
+        long buckets = configuration.get("buckets", 10L);
 
-        Stream<SimilarityResult> stream = topN(similarityStream(inputs, computer, configuration, similarityCutoff, topK), topN)
-                .map(SimilarityResult::squareRooted);
+        LSHSuperBit lsh = new LSHSuperBit(1, (int) buckets, n);
 
+        IdMap idMapping = new IdMap(inputs.length);
+        Stream<BucketResult> bucketStream = Stream.of(inputs).peek(item -> idMapping.add(item.id)).map(input -> {
+            int[] hash = lsh.hash(input.weights);
+            return new BucketResult(input.id, hash[0]);
+        });
 
-        boolean write = configuration.isWriteFlag(false) && similarityCutoff > 0.0;
-        return writeAndAggregateResults(configuration, stream, inputs.length, write);
+        TerminationFlag terminationFlag = TerminationFlag.wrap(transaction);
+
+        boolean shouldWrite = configuration.isWriteFlag(true);
+        String propertyName = configuration.getWriteProperty("bucket");
+        if (shouldWrite) {
+
+            int[] labels = bucketStream.map(item ->item.bucket).mapToInt(Math::toIntExact).toArray();
+            idMapping.buildMappedIds();
+
+            Exporter.of(idMapping, dbAPI)
+                    .withLog(log)
+                    .parallel(Pools.DEFAULT, configuration.getConcurrency(), terminationFlag)
+                    .build()
+                    .write(propertyName,
+                            labels,
+                            Translators.INT_ARRAY_TRANSLATOR
+                    );
+
+        }
+
+        return Stream.of(new BucketSummaryResult(inputs.length, shouldWrite, propertyName));
     }
 
 
