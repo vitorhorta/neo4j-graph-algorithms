81b0783add3c6c7717540013a0be8417375a0da3
Mark Needham <mark.needham@neotechnology.com>
2018-09-27 07:46:20 +0100

    common base class

--- algo/src/main/java/org/neo4j/graphalgo/impl/pagerank/WeightedComputeStepFactory.java
+++ algo/src/main/java/org/neo4j/graphalgo/impl/pagerank/WeightedComputeStepFactory.java
@@ -4,7 +4,7 @@ import org.neo4j.graphalgo.api.Degrees;
 import org.neo4j.graphalgo.api.RelationshipIterator;
 import org.neo4j.graphalgo.api.RelationshipWeights;
 
-public class WeightedComputeStepFactory implements  ComputeStepFactory {
+public class WeightedComputeStepFactory implements ComputeStepFactory {
     public ComputeStep createComputeStep(double dampingFactor, int[] sourceNodeIds, RelationshipIterator relationshipIterator, Degrees degrees, RelationshipWeights relationshipWeights, int partitionCount, int start) {
         return new WeightedComputeStep(
                 dampingFactor,
